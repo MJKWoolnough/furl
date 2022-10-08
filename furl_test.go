af1a14ccc5ca51691a8f2bc0b10986d40f5c3379
Michael Woolnough <michael.woolnough@gmail.com>
2022-10-08 12:31:40 +0100

    added GET tests

--- furl_test.go
+++ furl_test.go
@@ -35,3 +35,37 @@ func TestOptions(t *testing.T) {
 		}
 	}
 }
+
+func TestGet(t *testing.T) {
+	f := New(MemStore(map[string]string{
+		"AAA": "http://www.google.com",
+	}))
+	for n, test := range [...]struct {
+		Path, Location string
+		Status         int
+	}{
+		{
+			Path:     "/",
+			Location: "",
+			Status:   http.StatusNotFound,
+		},
+		{
+			Path:     "/AAA",
+			Location: "http://www.google.com",
+			Status:   http.StatusMovedPermanently,
+		},
+		{
+			Path:     "/BBB",
+			Location: "",
+			Status:   http.StatusNotFound,
+		},
+	} {
+		w := httptest.NewRecorder()
+		f.ServeHTTP(w, httptest.NewRequest(http.MethodGet, test.Path, nil))
+		if w.Code != test.Status {
+			t.Errorf("test %d: expecting response code %d, got %d", n+1, test.Status, w.Code)
+		} else if url := w.Header().Get("Location"); url != test.Location {
+			t.Errorf("test %d: expecting Location header to be %q, got %q", n+1, test.Location, url)
+		}
+	}
+}
