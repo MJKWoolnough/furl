18f33868430f33162f4113e7aa5cd25c1fd668da
Michael Woolnough <michael.woolnough@gmail.com>
2022-10-08 08:25:31 +0100

    start of url redirector

---
+++ furl.go
@@ -0,0 +1,31 @@
+package furl
+
+import (
+	"net/http"
+	"sync"
+)
+
+type Furl struct {
+	mu   sync.RWMutex
+	urls map[string]string
+}
+
+func (f *Furl) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet, http.MethodHead:
+		f.get(w, r)
+	case http.MethodPost:
+		f.post(w, r)
+	case http.MethodOptions:
+		f.options(w)
+	}
+}
+
+func (f *Furl) get(w http.ResponseWriter, r *http.Request) {
+}
+
+func (f *Furl) post(w http.ResponseWriter, r *http.Request) {
+}
+
+func (f *Furl) options(w http.ResponseWriter) {
+}
