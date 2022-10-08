00cfae12866d14a8096c28b91ed1b92b69213bc2
Michael Woolnough <michael.woolnough@gmail.com>
2022-10-08 19:14:52 +0100

    added tests for different content types

--- furl_test.go
+++ furl_test.go
@@ -95,41 +95,90 @@ func TestPost(t *testing.T) {
 		Body, Key, ContentType, Response string
 		Status                           int
 	}{
-		{
+		{ // 1
 			Body:        "ftp://google.com",
 			ContentType: "unknown",
 			Response:    unrecognisedContentType,
 			Status:      http.StatusUnsupportedMediaType,
 		},
-		{
+		{ // 2
 			Body:        "ftp://google.com",
 			ContentType: "text/plain",
 			Response:    invalidURL,
 			Status:      http.StatusBadRequest,
 		},
-		{
+		{ // 3
 			Body:        "http://google.com",
 			ContentType: "text/plain",
 			Response:    "AQ",
 			Status:      http.StatusOK,
 		},
-		{
+		{ // 4
 			Body:        "http://google.com",
 			ContentType: "text/plain",
 			Response:    "Ag",
 			Status:      http.StatusOK,
 		},
-		{
+		{ // 5
 			Body:        "http://google.com",
 			Key:         "ABC",
 			ContentType: "text/plain",
 			Response:    invalidKey,
 			Status:      http.StatusUnprocessableEntity,
 		},
+		{ // 6
+			Body:        `{"url":"http://google.com"}`,
+			Key:         "ABC",
+			ContentType: "application/json",
+			Response:    `{"error":"` + invalidKey + "\"}",
+			Status:      http.StatusUnprocessableEntity,
+		},
+		{ // 7
+			Body:        `<furl><url>http://google.com</url></furl>`,
+			Key:         "ABC",
+			ContentType: "text/xml",
+			Response:    "<furl><error>" + invalidKey + "</error></furl>",
+			Status:      http.StatusUnprocessableEntity,
+		},
+		{ // 8
+			Body:        "url=http://google.com",
+			Key:         "ABC",
+			ContentType: "application/x-www-form-urlencoded",
+			Response:    invalidKey,
+			Status:      http.StatusUnprocessableEntity,
+		},
+		{ // 9
+			Body:        `{"url":"http://google.com"}`,
+			Key:         "ABC",
+			ContentType: "application/json",
+			Response:    `{"error":"` + invalidKey + "\"}",
+			Status:      http.StatusUnprocessableEntity,
+		},
+		{ // 10
+			Body:        "<furl><url>http://google.com</url></furl>",
+			ContentType: "text/xml",
+			Response:    "<furl><key>AAA</key><url>http://google.com</url></furl>",
+			Status:      http.StatusOK,
+		},
+		{ // 11
+			Body:        `{"url":"http://api.microsoft.com"}`,
+			ContentType: "application/json",
+			Response:    `{"key":"AAAA","url":"http://api.microsoft.com"}`,
+			Status:      http.StatusOK,
+		},
+		{ // 12
+			Body:        "url=https://www.example.com",
+			ContentType: "application/x-www-form-urlencoded",
+			Response:    "AAAAAA",
+			Status:      http.StatusOK,
+		},
 	} {
 		w := httptest.NewRecorder()
 		r := httptest.NewRequest(http.MethodPost, "/"+test.Key, strings.NewReader(test.Body))
 		r.Header.Set("Content-Type", test.ContentType)
+		if test.ContentType == "application/x-www-form-urlencoded" {
+			test.ContentType = "text/html"
+		}
 		f.ServeHTTP(w, r)
 		if w.Code != test.Status {
 			t.Errorf("test %d: expecting response code %d, got %d", n+1, test.Status, w.Code)
